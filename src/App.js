5bb4d8bf2e8ff98db2809d35b30cec250277ca38
yishak <yishak.110402@gmail.com>
2024-06-09 10:59:48 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
-import { BrowserRouter, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import './index.css'
 import Navbar from "./components/Navbar/Navbar";
 import GeneralProvider from "./hooks/GeneralContext";
+import Homepage from "./pages/Homepage";
 export default function App() {
   return (
     <GeneralProvider>
     <BrowserRouter>
       <Navbar />
       <Routes>
-
+        <Route element={<Homepage/>} index/>
       </Routes>
     </BrowserRouter>
     </GeneralProvider>
