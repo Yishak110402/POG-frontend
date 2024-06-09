9f3def42cc25e9000e8ba212367008c5c24ced9a
yishak <yishak.110402@gmail.com>
2024-06-09 23:05:01 +0300

    first commit

--- src/components/Homepage/Hero/Hero.js
+++ src/components/Homepage/Hero/Hero.js
@@ -8,7 +8,7 @@ export default function Hero() {
     <div className="hero">
       <div className="hero-media">
         {/* <video src={heroVideo} loop muted autoPlay /> */}
-        <img src={heroImage} alt="Hospital image"/>
+        <img src={heroImage} alt="Hospital"/>
       </div>
       <div className="hero-text">
         <div className="hero-header">
