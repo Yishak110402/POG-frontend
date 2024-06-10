2e9bb154eb7c4302f9daae934916768e729c2ad9
yishak <yishak.110402@gmail.com>
2024-06-10 22:41:31 +0300

    first commit

---
+++ src/components/Homepage/Projects/Projects.js
@@ -0,0 +1,17 @@
+import './Projects.css'
+export default function Projects() {
+  return (
+    <div className='projects'>
+        <div className='shade'></div>
+      <h1>Our Latest Projects</h1>
+      <p>
+        Our first project is to equip Butajira General Hospital. Butajira
+        Hospital is a general hospital located in Ethiopia of Gurage zone and
+        surrounding areas in an average radius of 75 km which includes; most
+        part of Gurage zone, part of Silti and Hadiya zone, some parts of Oromia
+        region and other neighboring communities.
+      </p>
+      <button>Learn More</button>
+    </div>
+  );
+}
--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -2,16 +2,18 @@ import { useEffect } from "react";
 import Hero from "../components/Homepage/Hero/Hero";
 import OurMission from "../components/Homepage/OurMission/OurMission";
 import Subscription from "../components/Homepage/Subscription/Subscription";
+import Projects from "../components/Homepage/Projects/Projects";
 
 export default function Homepage(){
     useEffect(function(){
         document.title = "Home | Providence of Grace"
     },[])
     return(
-       <div>
+       <main>
         <Hero/>
         <OurMission/>
         <Subscription/>
-       </div>
+        <Projects/>
+       </main>
     )
 }
