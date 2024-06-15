1238529c17e428ce1f5a6c54dc0deb994fd76fac
yishak <yishak.110402@gmail.com>
2024-06-15 12:58:49 +0300

    first commit

--- src/hooks/GeneralContext.js
+++ src/hooks/GeneralContext.js
@@ -20,7 +20,8 @@ export default function GeneralProvider({ children }) {
       name: "Collecting Used Equipments From Hospitals In Dallas",
       location: "Dallas",
       time: "Time is TBD",
-      description:"Join us for an impactful community event focused on gathering and repurposing used medical equipment from hospitals across Dallas. This initiative aims to support healthcare facilities in under-resourced regions and contribute to sustainable healthcare practices.",
+      description:
+        "Join us for an impactful community event focused on gathering and repurposing used medical equipment from hospitals across Dallas. This initiative aims to support healthcare facilities in under-resourced regions and contribute to sustainable healthcare practices.",
       date: {
         date: 14,
         month: "July",
@@ -31,33 +32,43 @@ export default function GeneralProvider({ children }) {
       name: "Fundraising in the Coming Summer",
       location: "Location is TBD",
       time: "Time is TBD",
-      description:"Get ready for a season of fun and philanthropy with our Summer Fundraising Extravaganza! This event is designed to bring the community together to support Providence of Grace, while enjoying a variety of exciting activities under the summer sun.",
-      date:{
-        date:25,
-        month:"July",
-        year:2024
-      }
+      description:
+        "Get ready for a season of fun and philanthropy with our Summer Fundraising Extravaganza! This event is designed to bring the community together to support Providence of Grace, while enjoying a variety of exciting activities under the summer sun.",
+      date: {
+        date: 25,
+        month: "July",
+        year: 2024,
+      },
     },
     {
-      name: "Event Name",
+      name: "Carnival for Care",
+      date: { date: "15", month: "July", year: "2024" },
+      description:
+        "Join us for a day of fun and philanthropy at our Carnival for Care. Enjoy carnival games, rides, food stalls, and live entertainment. All proceeds support the collection and shipment of medical equipment to rural areas in Ethiopia, enhancing healthcare access for underserved communities.",
       location: "Location is TBD",
       time: "Time is TBD",
     },
     {
-      name: "Event Name",
+      name: "Sunset Beach Medical Aid Fundraiser",
+      date: { date: "5", month: "August", year: "2024" },
       location: "Location is TBD",
       time: "Time is TBD",
+      description:
+        "Spend an evening at our Sunset Beach Medical Aid Fundraiser, featuring a bonfire, and a silent auction. Funds raised will go towards collecting and sending vital medical equipment to rural Ethiopian clinics, improving healthcare delivery in remote regions.",
     },
     {
-      name: "Event Name",
+      name: "Community BBQ for Ethiopia's Health",
+      date: { date: "30", month: "June", year: "2024" },
       location: "Location is TBD",
       time: "Time is TBD",
+      description:
+        "Attend our Community BBQ for Ethiopia's Health for delicious food, live music, and family-friendly activities. Proceeds will benefit efforts to collect and refurbish medical equipment, ensuring rural Ethiopian healthcare facilities receive the tools they need to serve their communities effectively.",
     },
   ]);
 
-  function sendMessage(e){
-    e.preventDefault()
-    alert("Message Sent")
+  function sendMessage(e) {
+    e.preventDefault();
+    alert("Message Sent");
   }
   return (
     <GeneralContext.Provider
@@ -66,7 +77,7 @@ export default function GeneralProvider({ children }) {
         setNavOpen,
         events,
         setEvents,
-        sendMessage
+        sendMessage,
       }}>
       {children}
     </GeneralContext.Provider>
