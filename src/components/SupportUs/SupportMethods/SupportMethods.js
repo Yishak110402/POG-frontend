454e7a4c067cb53f6f6de43fd4ee16d28ed38719
yishak <yishak.110402@gmail.com>
2024-06-16 18:35:31 +0300

    first commit

--- src/components/SupportUs/SupportMethods/SupportMethods.js
+++ src/components/SupportUs/SupportMethods/SupportMethods.js
@@ -3,7 +3,7 @@ export default function SupportMethods() {
   return (
     <div className="support-methods">
       <div className="volunteer method">
-        <div className="shade"/>
+        <div className="shade" />
         <div className="volunteer-txt">
           <h1>Volunteering</h1>
           <p>
@@ -24,28 +24,28 @@ export default function SupportMethods() {
             greater than yourself.
           </p>
         </div>
+      </div>
+      <div className="fundraise method">
+        <div className="shade" />
+        <div className="fundraise-txt">
+          <h1>Fundraising</h1>
+          <p>
+            Fundraising is another impactful way to support Providence of Grace
+            Inc. and the individuals we serve. You can get involved by
+            organizing your own fundraising event or by participating in our
+            existing fundraising activities. Every dollar raised helps us
+            provide critical medical equipment and support services to those who
+            need it most.
+          </p>
+          <p>
+            We encourage you to bring your creativity and passion to our
+            fundraising efforts. Whether it's a community bake sale, a charity
+            run, or a silent auction, your contribution can make a significant
+            difference. Contact us with your fundraising ideas and help us
+            continue to make a positive impact on countless lives.
+          </p>
         </div>
-        <div className="fundraise method">
-            <div className="shade"/>
-          <div className="fundraise-txt">
-            <h1>Fundraising</h1>
-            <p>
-              Fundraising is another impactful way to support Providence of
-              Grace Inc. and the individuals we serve. You can get involved by
-              organizing your own fundraising event or by participating in our
-              existing fundraising activities. Every dollar raised helps us
-              provide critical medical equipment and support services to those
-              who need it most.
-            </p>
-            <p>
-              We encourage you to bring your creativity and passion to our
-              fundraising efforts. Whether it's a community bake sale, a charity
-              run, or a silent auction, your contribution can make a significant
-              difference. Contact us with your fundraising ideas and help us
-              continue to make a positive impact on countless lives.
-            </p>
-          </div>
-        </div>
+      </div>
     </div>
   );
 }
