f46d6ef246dce94ac154a20e902f0cc664082748
yishak <yishak.110402@gmail.com>
2024-06-16 20:27:21 +0300

    first commit

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -15,12 +15,12 @@ export default function Footer() {
       </div>
       <nav>
         <ul className="footer-links">
-          <Link>Home</Link>
-          <Link>Who We Are</Link>
-          <Link>Support Us</Link>
-          <Link>Events</Link>
-          <Link>News</Link>
-          <Link>Contact</Link>
+          <Link to={"/"}>Home</Link>
+          <Link to={"/who-we-are"}>Who We Are</Link>
+          <Link to={"support-us"}>Support Us</Link>
+          <Link to={"/events"}>Events</Link>
+          <Link to={"/news"}>News</Link>
+          <Link to={"/contact"}>Contact</Link>
         </ul>
       </nav>
     </footer>
